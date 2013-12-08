6ad559a0b99d97bcda6d6dafa6eec1a993910091
mpavel <mmbpavel@gmail.com>
2013-12-08 17:01:52 +1100

    Updated license information and code template

--- vdntemplate/src/main/java/org/mpavel/app/security/ApplicationSecurityInterceptor.java
+++ vdntemplate/src/main/java/org/mpavel/app/security/ApplicationSecurityInterceptor.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2013 Muhammad Pavel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
 package org.mpavel.app.security;
 
 import java.lang.reflect.Method;
@@ -20,6 +36,12 @@ import com.vaadin.ui.Notification;
 import com.vaadin.ui.Notification.Type;
 import com.vaadin.ui.UI;
 
+/**
+ * I...
+ *
+ * @author mpavel
+ *
+ */
 public class ApplicationSecurityInterceptor implements MethodInterceptor
 {
 	private static final ApplicationLogger logger = new ApplicationLogger(ApplicationSecurityInterceptor.class);
--- vdntemplate/src/main/java/org/mpavel/app/views/ApplicationView.java
+++ vdntemplate/src/main/java/org/mpavel/app/views/ApplicationView.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2013 Muhammad Pavel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
 package org.mpavel.app.views;
 
 import org.mpavel.app.security.ApplicationSecurity;
@@ -12,6 +28,12 @@ import com.vaadin.ui.Panel;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
+/**
+ * I...
+ *
+ * @author mpavel
+ *
+ */
 public class ApplicationView extends Panel implements View
 {
 	private static final long serialVersionUID = -1307009854319440228L;
